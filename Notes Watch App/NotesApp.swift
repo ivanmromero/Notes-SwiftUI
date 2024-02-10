8d2e853f930deb34cd78db2360e139f1ea4bfd26
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-10 16:23:15 -0300

    Added initial config

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Ivan Romero on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
